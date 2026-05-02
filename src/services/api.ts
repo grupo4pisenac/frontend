90f6ccc9ba6ba6b936ab903a1d9d567ed9d7a416
Arthur Bem <ARTHURHCBEM@GMAIL.COM>
2026-05-02 19:39:33 -0300

    Update api.ts
    Mudanças pra começar a integração e o console do inspecionar informar mais detalhes sobre o erro

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,21 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080', // Porta padrão do Java
+  baseURL: 'http://localhost:8080',
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('@EduManage:token');
+  
+  // O NOSSO ESPIÃO:
+  console.log("Interceptor da API disparado!");
+  console.log("Token encontrado no navegador?", token ? "SIM! (Injetando...)" : "NÃO! (O token está vazio)");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
