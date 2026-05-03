005d34fb5b853c39a43e09e1d2d69c0670b912b6
Luiz44141 <luizeduardomaranhao1@gmail.com>
2026-05-02 23:44:22 -0300

    ajuste do endpoint para o link do back no railway

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080',
+
+  baseURL: 'https://backend-production-a784.up.railway.app', 
 });
 
 api.interceptors.request.use((config) => {
